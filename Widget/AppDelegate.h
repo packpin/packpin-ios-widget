3f98eb0d0c9f9586170e2bbbf4fb5100abc90aba
Marius <marius@recafe.lt>
2015-06-29 14:37:16 +0300

    Initial Commit

---
+++ Widget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Widget
+//
+//  Created by Marius on 29/06/15.
+//  Copyright (c) 2015 Packpin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Widget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Widget
+//
+//  Created by Marius on 29/06/15.
+//  Copyright (c) 2015 Packpin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
